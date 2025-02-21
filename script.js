8dce8ddce98a449fd6ca0cdbf41fcb8d2de53ee8
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2025-02-21 12:32:37 +0300

    Add a subtraction function
    This will handle all the subtraction opertations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function addNumbers(...numbers){
    return numbers.reduce((sum , num) => sum+num ,0);
 }
+function subtractNumbers(...numbers){
+    return numbers.reduce((subtract, num)=> subtract-num);
+}
