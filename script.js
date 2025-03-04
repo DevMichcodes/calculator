052976de4e89bf59b83b9660e4bbecfe92d807b5
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2025-03-04 12:36:19 +0300

    Add function to compute results
    This allows the calculator to show the result of
    each calculation

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ display.value = display.value.slice(0,-1);
  function clearDisplay(){
     display.value = "";
  }
+ function calculateResult(){
+    try{
+        display.value = evaluateExpression(display.value);
+    }
+    catch(error)
+    {
+   display.value ="Error";
+    }
+  
+ }
+ function evaluateExpression(expression){
+    expression = expression.replace(/%/g, "/100")
+
+      // Ensure only valid characters are processed
+      if (/^[0-9+\-*/.% ^\s]+$/.test(expression)) {
+        return new Function("return " + expression)(); // Safely evaluate
+    } else {
+        throw new Error("Invalid Expression");
+    }
+
+ }
